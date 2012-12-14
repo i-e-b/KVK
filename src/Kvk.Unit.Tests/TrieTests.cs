fa1be3c368f941f252625ea7f23318cbef2e78f8
Iain Ballard <iain.ballard@7digital.com>
2012-12-14 13:16:42 +0000

    Starting to wrap trie in tests

---
+++ src/Kvk.Unit.Tests/TrieTests.cs
@@ -0,0 +1,46 @@
+﻿using KVK.Core;
+using NUnit.Framework;
+
+namespace Kvk.Unit.Tests
+{
+	[TestFixture]
+    public class TrieTests
+    {
+		Trie<object> subject;
+
+		[SetUp]
+		public void A_trie_with_some_values ()
+		{
+			subject = new Trie<object>();
+
+			subject.Add("polyglot", "one");
+			subject.Add("polyhedron", "two");
+			subject.Add("polyglottal", "three");
+		}
+
+		[Test]
+		public void Can_find_a_stored_value_based_on_a_key_string ()
+		{
+			Assert.That(subject.Find("polyglot"), Is.EqualTo("one"));
+			Assert.That(subject.Find("polyhedron"), Is.EqualTo("two"));
+		}
+
+		[Test]
+		public void Can_find_a_node_based_on_a_common_prefix()
+		{
+			Assert.That(subject.FindNode("poly"), Is.Not.Null);
+		}
+
+		[Test]
+		public void Finding_a_node_based_on_a_non_existent_prefix_returns_null()
+		{
+			Assert.That(subject.FindNode("mono"), Is.Null);
+		}
+
+		[Test]
+		public void Can_find_all_node_values_in_a_string_path ()
+		{
+			Assert.That(subject.FindAll("polyglottal"), Is.EquivalentTo(new []{"one", "three"}));
+		}
+    }
+}
